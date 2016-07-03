8800cf7fb9388399ff1806bbcad2d697a8564888
Squid767 <paul.perrone@stickmanventures.com>
2016-07-03 14:20:08 -0700

    update

--- serve.py
+++ serve.py
@@ -4,16 +4,19 @@ from sys import exit
 from config.config import config
 import lib.bot as bot
 from lib.serial_start import serialFeed1
+import pygame
 
 # twitch-IRC
 # Inspired by http://twitch.tv/twitchplayspokemon
 # Initialized from a build of Twitch Plays by Aidan Thomson
+# Original github repository: https://github.com/aidanrt/twitch-plays 
 # ---
 # Additional Functionality added by Paul Perrone
 #
 # Ability to turn the bot off and on mid stream
 # 	-Uses a specific message in chat by a specific twitch account
 # Communication with Arduino through serial ports
+# 	-Controller support for the same keymap commands
 # Chat data collection into a csv for analysis
 #
 
